a445fdeb638376cc90c470f23f1833dc08fba85b
Arek Burdach <arek.burdach@gmail.com>
2014-07-10 22:48:45 +0200

    prościej

--- src/main/scala/ready/p08/DeclarativeProgramming.scala
+++ src/main/scala/ready/p08/DeclarativeProgramming.scala
@@ -2,12 +2,12 @@ package ready.p08
 
 class DeclarativeProgramming extends App {
   val ducks = List(
-    new Duck("Donald", "m", 20),
-    new Duck("Sknerus McKwacz", "m", 60),
-    new Duck("Della", "f", 30),
-    new Duck("Hyzio", "m", 5),
-    new Duck("Zyzio", "m", 5),
-    new Duck("Dyzio", "m", 5)
+    Duck("Donald", "m", 20),
+    Duck("Sknerus McKwacz", "m", 60),
+    Duck("Della", "f", 30),
+    Duck("Hyzio", "m", 5),
+    Duck("Zyzio", "m", 5),
+    Duck("Dyzio", "m", 5)
   )
 
   val adultMalesNames = findAdultMalesNames(ducks)
